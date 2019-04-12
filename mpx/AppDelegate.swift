7a866eb1f691ecbc07fdfa1ae52df713bc3b75ea
mtdcy.chen <mtdcy.chen@gmail.com>
2019-04-12 17:06:34 +0800

    Initial Commit

---
+++ mpx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mpx
+//
+//  Created by Chen Fang on 2019/4/12.
+//  Copyright © 2019 Chen Fang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mpx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mpx
+//
+//  Created by Chen Fang on 2019/4/12.
+//  Copyright © 2019 Chen Fang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
